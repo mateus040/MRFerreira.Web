80a08119d1b8364cfeae5d6fd795d35c061b619e
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-07-20 14:08:50 -0300

    chore

--- src/pages/Admin/components/header.tsx
+++ src/pages/Admin/components/header.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { FaBars, FaRegUser } from "react-icons/fa6";
-import { IoIosArrowDown } from "react-icons/io";
+import { FaBars } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 interface Props {
