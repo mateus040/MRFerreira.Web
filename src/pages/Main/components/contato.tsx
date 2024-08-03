1de73f1429379e26b0b8569d6388bcfdbf6187a5
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-08-03 14:11:53 -0300

    wip: requisição contato

--- src/pages/Main/components/contato.tsx
+++ src/pages/Main/components/contato.tsx
@@ -1,7 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
 
+interface EmailPostArgs {
+  nome: string;
+  email: string;
+  descricao: string;
+}
+
 export default function Contato() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EmailPostArgs>();
+
+  const onSubmit: SubmitHandler<EmailPostArgs> = async (data) => {
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("nome", data.nome);
+    formData.append("email", data.email);
+    formData.append("descricao", data.descricao);
+
+    toast
+      .promise(
+        axios.post(
+          "https://mrferreira-api.vercel.app/api/api/send-email",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        ),
+        {
+          loading: "Enviando e-mail...",
+          success: "E-mail enviado com sucesso!",
+          error: (error) => {
+            if (axios.isAxiosError(error)) {
+              return (
+                "Erro de solicitação: " +
+                (error.response?.data || error.message)
+              );
+            } else if (error instanceof Error) {
+              return "Erro desconhecido: " + error.message;
+            } else {
+              return "Erro inesperado: " + error;
+            }
+          },
+        }
+      )
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="px-8 lg:px-12 py-12 container mx-auto" id="contato">
       <div className="flex flex-col items-center justify-center">
@@ -48,29 +107,49 @@ export default function Contato() {
 
       {/*<div className="flex flex-col items-center justify-center mt-5 bg-white p-8 rounded-md">*/}
       {/*<p className="text-2xl font-semibold mb-5">Envie um e-mail</p>*/}
-      <form className="w-full mt-3">
+      <form className="w-full mt-3" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          name="nome"
+          id="nome"
+          {...register("nome", { required: "O nome é obrigatório" })}
           placeholder="Digite seu nome"
-          className="bg-white  w-full rounded-md p-3 mb-6"
+          className={`bg-white w-full rounded-md p-3 mb-6 ${
+            errors.nome ? "border border-red-500" : ""
+          }`}
         />
+        {errors.nome && (
+          <p className="text-red-500 text-sm">{errors.nome.message}</p>
+        )}
         <input
           type="email"
-          name="email"
+          id="email"
+          {...register("email", { required: "O e-mail é obrigatório" })}
           placeholder="Digite seu e-mail"
-          className="bg-white w-full rounded-md p-3 mb-6"
+          className={`bg-white w-full rounded-md p-3 mb-6 ${
+            errors.email ? "border border-red-500" : ""
+          }`}
         />
+        {errors.email && (
+          <p className="text-red-500 text-sm">{errors.email.message}</p>
+        )}
         <textarea
-          name="descricao"
+          id="descricao"
           placeholder="Digite a descrição"
+          {...register("descricao", { required: "A descrição é obrigatória" })}
           rows={10}
-          className="bg-white w-full rounded-md p-3 mb-6"
+          className={`bg-white w-full rounded-md p-3 mb-6 ${
+            errors.descricao ? "border border-red-500" : ""
+          }`}
         />
+        {errors.descricao && (
+          <p className="text-red-500 text-sm">{errors.descricao.message}</p>
+        )}
+
         <div className="flex justify-end mt-4">
           <button
             type="submit"
             className="border-2 border-black rounded px-8 py-2 hover:bg-black hover:text-white transition-all"
+            disabled={loading}
           >
             Enviar
           </button>
